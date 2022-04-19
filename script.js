8eaf2f497f67e54753122484d6b27faf8ca31258
CodePiru <coding.jose@gmail.com>
2022-04-19 15:53:16 -0500

    Change pixel borders for grid-gap / style details

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const container = document.querySelector('#container')
 function createGrid (num) {
     container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
     container.innerHTML = '';
-    for (let i = 1; i < (num * num); i++) {
+    for (let i = 1; i <= (num * num); i++) {
         const div = document.createElement('div');
         div.classList.add('pixel');
         container.appendChild(div);
