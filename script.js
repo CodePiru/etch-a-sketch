774e00ef9afc27d6fa498e829ddb002dca619b38
CodePiru <coding.jose@gmail.com>
2022-04-22 09:37:58 -0500

    Add changeGrid feature and nav buttons

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const container = document.querySelector('#container')
+const container = document.querySelector('#container');
+const changeGrid = document.querySelector('#change-grid');
+const modes = document.querySelectorAll('.mode');
 
 function createGrid (num) {
     container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
@@ -13,6 +15,15 @@ function createGrid (num) {
 function normalMode (e) {
     e.target.classList.add('active');
 }
+
+changeGrid.addEventListener('click', () => {
+    const number = parseInt(prompt('¿How many squares (per side) do you want?\nFrom 2 to 100'));
+    if (number >= 2 && number <= 100) {
+        createGrid(number);
+    } else {
+        alert('Invalid option');
+    };
+})
 
 container.addEventListener('mouseover', normalMode)
 
